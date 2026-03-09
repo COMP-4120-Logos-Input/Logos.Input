95600683abc7875a2b3470b251528d3e85f6e481
PixelRican <rimercado02@gmail.com>
2026-03-09 11:33:05 -0400

    Create IInputDevice.cs

---
+++ Source/IInputDevice.cs
@@ -0,0 +1,17 @@
+﻿namespace Logos.Input
+{
+    /// <summary>
+    /// Defines methods that report the connection status of an input device.
+    /// </summary>
+    public interface IInputDevice
+    {
+        /// <summary>
+        /// Returns a value that indicates whether the <see cref="IInputDevice"/> is connected.
+        /// </summary>
+        /// <returns>
+        /// <see langword="true"/> if the <see cref="IInputDevice"/> is connected; otherwise,
+        /// <see langword="false"/>.
+        /// </returns>
+        bool IsConnected { get; }
+    }
+}
