291511f03d4051e60954917c63be01bb0a36bdb3
PixelRican <rimercado02@gmail.com>
2026-03-11 10:23:47 -0400

    Create SDL_KeyboardEvent.cs

---
+++ Source/Sdl3/SDL_KeyboardEvent.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Logos.Input.Sdl3
+{
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct SDL_KeyboardEvent
+    {
+        public SDL_EventType type;
+        public uint reserved;
+        public ulong timestamp;
+        public uint windowID;
+        public uint which;
+        public SDL_Scancode scancode;
+        public SDL_Keycode key;
+        public SDL_Keymod mod;
+        public ushort raw;
+        public byte down;
+        public byte repeat;
+    }
+}
