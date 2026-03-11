e262e9364287b52affb41bba9bc8e45f0e66203a
PixelRican <rimercado02@gmail.com>
2026-03-11 12:22:56 -0400

    Create IKeyboardDevice interface

---
+++ Source/IKeyboardDevice.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Logos.Input
+{
+    public interface IKeyboardDevice : IInputDevice
+    {
+        event EventHandler<KeyboardEventArgs> KeyPressed;
+
+        event EventHandler<KeyboardEventArgs> KeyReleased;
+
+        bool IsKeyDown(KeyCode key);
+    }
+}
---
+++ Source/KeyboardEventArgs.cs
@@ -0,0 +1,21 @@
+﻿namespace Logos.Input
+{
+    public readonly record struct KeyboardEventArgs
+    {
+        public KeyboardEventArgs(IKeyboardDevice source, long timestamp, KeyCode key, bool isRepeat)
+        {
+            Source = source;
+            Timestamp = timestamp;
+            Key = key;
+            IsRepeat = isRepeat;
+        }
+
+        public IKeyboardDevice Source { get; }
+
+        public long Timestamp { get; }
+
+        public KeyCode Key { get; }
+
+        public bool IsRepeat { get; }
+    }
+}
