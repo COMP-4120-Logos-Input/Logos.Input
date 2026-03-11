eb74246b88093181475bf2d60953dc84ec5baa95
PixelRican <rimercado02@gmail.com>
2026-03-11 10:30:52 -0400

    Create SDL_CommonEvent.cs

---
+++ Source/Sdl3/SDL_CommonEvent.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Logos.Input.Sdl3
+{
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct SDL_CommonEvent
+    {
+        public SDL_EventType type;
+        public uint reserved;
+        public ulong timestamp;
+    }
+}
