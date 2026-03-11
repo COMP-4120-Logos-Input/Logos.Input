fe1a9e353dc89c3475a83edb585523665c6f1355
PixelRican <rimercado02@gmail.com>
2026-03-11 10:04:17 -0400

    Create SDL_QuitEvent.cs

---
+++ Source/Sdl3/SDL_QuitEvent.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Logos.Input.Sdl3
+{
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct SDL_QuitEvent
+    {
+        public SDL_EventType type;
+        public uint reserved;
+        public ulong timestamp;
+    }
+}
