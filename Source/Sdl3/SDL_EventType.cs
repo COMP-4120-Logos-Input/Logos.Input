57d203bca58a976b804af60653d9acf6132deda9
PixelRican <rimercado02@gmail.com>
2026-03-11 10:03:55 -0400

    Create SDL_EventType.cs

---
+++ Source/Sdl3/SDL_EventType.cs
@@ -0,0 +1,13 @@
+﻿namespace Logos.Input.Sdl3
+{
+    internal enum SDL_EventType : uint
+    {
+        SDL_EVENT_FIRST = 0,
+        SDL_EVENT_QUIT = 0x100,
+        SDL_EVENT_KEY_DOWN = 0x300,
+        SDL_EVENT_KEY_UP,
+        SDL_EVENT_KEYMAP_CHANGED,
+        SDL_EVENT_KEYBOARD_ADDED,
+        SDL_EVENT_KEYBOARD_REMOVED
+    }
+}
