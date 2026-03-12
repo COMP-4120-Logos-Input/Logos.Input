68dc630a839f37b684ff68ea4006e9a59bd77706
PixelRican <rimercado02@gmail.com>
2026-03-12 10:16:38 -0400

    Update EventQueueMarshal.cs

--- Source/Sdl3/EventQueueMarshal.cs
+++ Source/Sdl3/EventQueueMarshal.cs
@@ -6,49 +6,49 @@ namespace Logos.Input.Sdl3
 {
     public static class EventQueueMarshal
     {
-        private static ulong GetTimestampAsNanoseconds()
-        {
-            return (ulong)Stopwatch.GetTimestamp() * TimeSpan.NanosecondsPerTick;
-        }
-
-        public static void PushKeyboardConnectedEvent(uint keyboardId)
+        public static void OnKeyboardConnected(int keyboardId)
         {
             SDL_Event e = default;
             e.kdevice.type = SDL_EventType.SDL_EVENT_KEYBOARD_ADDED;
             e.kdevice.timestamp = GetTimestampAsNanoseconds();
-            e.kdevice.which = keyboardId;
+            e.kdevice.which = (uint)keyboardId;
             SDL_PushEvent(ref e);
         }
 
-        public static void PushKeyboardDisconnectedEvent(uint keyboardId)
+        public static void OnKeyboardDisconnected(int keyboardId)
         {
             SDL_Event e = default;
             e.kdevice.type = SDL_EventType.SDL_EVENT_KEYBOARD_REMOVED;
             e.kdevice.timestamp = GetTimestampAsNanoseconds();
-            e.kdevice.which = keyboardId;
+            e.kdevice.which = (uint)keyboardId;
             SDL_PushEvent(ref e);
         }
 
-        public static void PushKeyPressedEvent(uint keyboardId, KeyCode key, bool isRepeat)
+        public static void OnKeyPressed(int keyboardId, KeyCode key, bool isRepeat)
         {
             SDL_Event e = default;
             e.key.type = SDL_EventType.SDL_EVENT_KEY_DOWN;
             e.key.timestamp = GetTimestampAsNanoseconds();
-            e.key.which = keyboardId;
+            e.key.which = (uint)keyboardId;
             e.key.scancode = (SDL_Scancode)key;
             e.key.down = 1;
             e.key.repeat = (byte)(isRepeat ? 1 : 0);
             SDL_PushEvent(ref e);
         }
 
-        public static void PushKeyReleasedEvent(uint keyboardId, KeyCode key)
+        public static void OnKeyReleased(int keyboardId, KeyCode key)
         {
             SDL_Event e = default;
             e.key.type = SDL_EventType.SDL_EVENT_KEY_UP;
             e.key.timestamp = GetTimestampAsNanoseconds();
-            e.key.which = keyboardId;
+            e.key.which = (uint)keyboardId;
             e.key.scancode = (SDL_Scancode)key;
             SDL_PushEvent(ref e);
+        }
+
+        private static ulong GetTimestampAsNanoseconds()
+        {
+            return (ulong)Stopwatch.GetTimestamp() * TimeSpan.NanosecondsPerTick;
         }
     }
 }
