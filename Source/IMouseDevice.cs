321eec7beb3831e1797cdf3ccf2b38eab28c1d4e
PixelRican <rimercado02@gmail.com>
2026-03-23 14:31:40 -0400

    Create IMouseDevice.cs

---
+++ Source/IMouseDevice.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Numerics;
+
+namespace Logos.Input
+{
+    public interface IMouseDevice : IInputDevice
+    {
+        Vector2 CursorPosition { get; }
+
+        Vector2 WheelRotation { get; }
+
+        event EventHandler<MouseButtonEventArgs> ButtonPressed;
+
+        event EventHandler<MouseButtonEventArgs> ButtonReleased;
+
+        event EventHandler<MouseWheelEventArgs> WheelRolled;
+
+        event EventHandler<MouseCursorEventArgs> CursorMoved;
+
+        bool IsButtonPressed(MouseButton button);
+    }
+}
