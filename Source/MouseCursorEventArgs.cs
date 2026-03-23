b05c772eabb69163537533fab46ce54e89f31c7a
PixelRican <rimercado02@gmail.com>
2026-03-23 14:27:04 -0400

    Create MouseCursorEventArgs.cs

---
+++ Source/MouseCursorEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System.Numerics;
+
+namespace Logos.Input
+{
+    public readonly record struct MouseCursorEventArgs
+    {
+        public MouseCursorEventArgs(Vector2 position, long timestamp)
+        {
+            Position = position;
+            Timestamp = timestamp;
+        }
+
+        public Vector2 Position { get; }
+
+        public long Timestamp { get; }
+    }
+}
