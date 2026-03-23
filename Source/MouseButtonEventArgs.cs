162edb69003a62cb1ed9c30e6a28ef25c767a02d
PixelRican <rimercado02@gmail.com>
2026-03-23 14:15:23 -0400

    Create MouseButtonEventArgs.cs

---
+++ Source/MouseButtonEventArgs.cs
@@ -0,0 +1,15 @@
+﻿namespace Logos.Input
+{
+    public readonly record struct MouseButtonEventArgs
+    {
+        public MouseButtonEventArgs(MouseButton button, long timestamp)
+        {
+            Button = button;
+            Timestamp = timestamp;
+        }
+
+        public MouseButton Button { get; }
+
+        public long Timestamp { get; }
+    }
+}
