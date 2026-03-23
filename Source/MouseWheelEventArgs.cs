14603f76e5064a9e986087cdc1f93420ea2c4d4f
PixelRican <rimercado02@gmail.com>
2026-03-23 14:22:14 -0400

    Create MouseWheelEventArgs.cs

---
+++ Source/MouseWheelEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System.Numerics;
+
+namespace Logos.Input
+{
+    public readonly record struct MouseWheelEventArgs
+    {
+        public MouseWheelEventArgs(Vector2 rotation, long timestamp)
+        {
+            Rotation = rotation;
+            Timestamp = timestamp;
+        }
+
+        public Vector2 Rotation { get; }
+
+        public long Timestamp { get; }
+    }
+}
