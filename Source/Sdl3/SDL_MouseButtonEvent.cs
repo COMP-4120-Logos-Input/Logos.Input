b48fa5b48a69037d6417a930361c45d32ab3ec83
Jumony <fireball2489@gmail.com>
2026-03-23 23:25:32 -0400

    SDL_MouseButtonEvent

---
+++ Source/Sdl3/SDL_MouseButtonEvent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Logos.Input.Sdl3
+{
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct SDL_MouseButtonEvent
+    {
+        public SDL_EventType type;
+        public UInt32 reserved;
+        public UInt64 timestamp;
+        public UInt32 windowID;  // SDL_WindowID
+        public UInt32 which;  // SDL_MouseID
+        public byte button;
+        public bool down;
+        public byte clicks;
+        public byte padding;
+        public float x;
+        public float y;
+    }
+}
