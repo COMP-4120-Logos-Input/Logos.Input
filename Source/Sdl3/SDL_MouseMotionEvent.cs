1b9583ef7ddf3802755a71d5d94394c7353681ac
Jumony <fireball2489@gmail.com>
2026-03-23 23:20:24 -0400

    SDL_MouseMotionEvent  Please enter the commit message for your changes. Lines starting

--- Source/Sdl3/SDL_MouseMotionEvent.cs
+++ Source/Sdl3/SDL_MouseMotionEvent.cs
@@ -6,6 +6,15 @@ namespace Logos.Input.Sdl3
     [StructLayout(LayoutKind.Sequential)]
     internal struct SDL_MouseMotionEvent
     {
-        // TODO
+        public SDL_EventType type;
+        public UInt32 reserved;
+        public UInt64 timestamp;
+        public UInt32 windowID;  // SDL_WindowID
+        public uint which;
+        public UInt32 state;  // SDL_MouseButtonFlags
+        public float x;
+        public float y;
+        public float xrel;
+        public float yrel;
     }
 }
