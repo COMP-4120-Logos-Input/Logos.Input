18305b329008fae38e03a3d7ff76f1bbd829a8e7
PixelRican <rimercado02@gmail.com>
2026-03-26 12:46:05 -0400

    Create IInputMapper.cs
    The IInputMapper interface is the base interface for all input mappers. It exposes methods that allows users to supply the input mapper with input devices, which will then listen for events emitted by the input device. This allows events raised by a category of input devices to be forwarded to event handlers.

---
+++ Source/IInputMapper.cs
@@ -0,0 +1,9 @@
+﻿namespace Logos.Input
+{
+    public interface IInputMapper<in TInputDevice> where TInputDevice : IInputDevice
+    {
+        void Connect(TInputDevice device);
+
+        void Disconnect(TInputDevice device);
+    }
+}
