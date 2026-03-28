38da0fd956ef1b3f6ca85fadab8ed496833f6a15
PixelRican <rimercado02@gmail.com>
2026-03-27 22:52:24 -0400

    Create mouse event factories
    The added classes provide some convenience methods for binding actions and states to mouse events.

---
+++ Source/MouseButtonEventHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Logos.Input
+{
+    public static class MouseButtonEventHandler
+    {
+        public static EventHandler<MouseButtonEventArgs> Create<TState>(Action<TState> action, TState state)
+        {
+            ArgumentNullException.ThrowIfNull(action);
+            return (_, _) => action(state);
+        }
+
+        public static EventHandler<MouseButtonEventArgs>  Create<TState>(Action<TState> action, Func<object?, MouseButtonEventArgs, TState> converter)
+        {
+            ArgumentNullException.ThrowIfNull(action);
+            ArgumentNullException.ThrowIfNull(converter);
+            return (sender, args) => action(converter(sender, args));
+        }
+    }
+}
