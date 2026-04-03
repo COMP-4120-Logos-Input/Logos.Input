d4a043d3de0ff298324b7939a93f12fc1ac30b6c
PixelRican <rimercado02@gmail.com>
2026-04-03 16:10:21 -0400

    Update MouseButtonEventArgs.cs

--- Source/MouseButtonEventArgs.cs
+++ Source/MouseButtonEventArgs.cs
@@ -2,9 +2,26 @@
 
 namespace Logos.Input
 {
+    /// <summary>
+    /// Provides data for mouse button events sent by mouse devices.
+    /// </summary>
     public class MouseButtonEventArgs : InputEventArgs
     {
-        public MouseButtonEventArgs(IMouseDevice device, MouseButton button, TimeSpan timestamp) : base(device, timestamp)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MouseButtonEventArgs"/> class that contains
+        /// event data sent by the specified mouse device at the specified time, which was triggered
+        /// by the specified mouse button.
+        /// </summary>
+        /// <param name="device">
+        /// The mouse device that sent the input event.
+        /// </param>
+        /// <param name="button">
+        /// The mouse button that triggered the input event.
+        /// </param>
+        /// <param name="timestamp">
+        /// The time at which the input event occured.
+        /// </param>
+        public MouseButtonEventArgs(IMouseDevice device, TimeSpan timestamp, MouseButton button) : base(device, timestamp)
         {
             Button = button;
         }
