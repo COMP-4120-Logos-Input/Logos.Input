c7712d250e5d46f685e778369fd86a5ae4c44f04
PixelRican <rimercado02@gmail.com>
2026-04-03 15:43:37 -0400

    Update InputEventArgs.cs

--- Source/InputEventArgs.cs
+++ Source/InputEventArgs.cs
@@ -1,15 +1,43 @@
-﻿namespace Logos.Input
+﻿using System;
+
+namespace Logos.Input
 {
-    public readonly record struct InputEventArgs
+    /// <summary>
+    /// Represents a base class for classes that contain event data emitted by connected input
+    /// devices.
+    /// </summary>
+    public class InputEventArgs : EventArgs
     {
-        public InputEventArgs(IInputDevice device, long timestamp)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InputEventArgs"/> class that contains event
+        /// data sent by the specified input device at the specified timestamp.
+        /// </summary>
+        /// <param name="device">
+        /// The input device that triggered the input event.
+        /// </param>
+        /// <param name="timestamp">
+        /// The time at which the input event occured.
+        /// </param>
+        public InputEventArgs(IInputDevice device, TimeSpan timestamp)
         {
             Device = device;
             Timestamp = timestamp;
         }
 
+        /// <summary>
+        /// Gets the input device that triggered the input event.
+        /// </summary>
+        /// <returns>
+        /// The input device that triggered the input event.
+        /// </returns>
         public IInputDevice Device { get; }
 
-        public long Timestamp { get; }
+        /// <summary>
+        /// Gets the time at which the input event occured.
+        /// </summary>
+        /// <returns>
+        /// The time at which the input event occured.
+        /// </returns>
+        public TimeSpan Timestamp { get; }
     }
 }
