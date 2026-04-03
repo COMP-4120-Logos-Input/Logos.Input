3d95f6cc18dc9837a683805a4e5f14c8e4fa28f2
PixelRican <rimercado02@gmail.com>
2026-04-03 14:58:06 -0400

    Create IInputListener.cs

---
+++ Source/IInputListener.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Logos.Input
+{
+    /// <summary>
+    /// Defines methods that access connected input devices and notify event handlers when their
+    /// connection status changes.
+    /// </summary>
+    public interface IInputListener
+    {
+        /// <summary>
+        /// Gets a collection of input devices that are currently connected.
+        /// </summary>
+        /// <returns>
+        /// A collection of input devices that are currently connected.
+        /// </returns>
+        IEnumerable<IInputDevice> ConnectedDevices { get; }
+
+        /// <summary>
+        /// Occurs when an input device is connected.
+        /// </summary>
+        event EventHandler<InputEventArgs> DeviceConnected;
+
+        /// <summary>
+        /// Occurs when an input device is disconnected.
+        /// </summary>
+        event EventHandler<InputEventArgs> DeviceDisconnected;
+    }
+}
