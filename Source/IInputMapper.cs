7d21fa3ab209fe7a0912b5d1bd4f8eae1d016f41
PixelRican <rimercado02@gmail.com>
2026-04-03 18:03:52 -0400

    Update IInputMapper.cs

--- Source/IInputMapper.cs
+++ Source/IInputMapper.cs
@@ -1,9 +1,38 @@
-﻿namespace Logos.Input
-{
-    public interface IInputMapper<in TInputDevice> where TInputDevice : IInputDevice
-    {
-        void Connect(TInputDevice device);
+﻿using System;
 
-        void Disconnect(TInputDevice device);
+namespace Logos.Input
+{
+    /// <summary>
+    /// Represents an event router that invokes event handlers based on mapped input data.
+    /// </summary>
+    public interface IInputMapper
+    {
+        /// <summary>
+        /// Routes events exposed by compatible input listeners contained by the specified input
+        /// provider to the <see cref="IInputMapper"/>.
+        /// </summary>
+        /// <param name="provider">
+        /// The input provider containing input listeners whose events can be routed to the
+        /// <see cref="IInputMapper"/>.
+        /// </param>
+        /// <exception cref="NotSupportedException">
+        /// <paramref name="provider"/> does not contain an input listener requested by the
+        /// <see cref="IInputMapper"/>.
+        /// </exception>
+        void Connect(IInputProvider provider);
+
+        /// <summary>
+        /// Blocks events exposed by compatible input listeners contained by the specified input
+        /// provider from reaching the <see cref="IInputMapper"/>.
+        /// </summary>
+        /// <param name="provider">
+        /// The input provider containing input listeners whose events are to be blocked from the
+        /// <see cref="IInputMapper"/>.
+        /// </param>
+        /// <exception cref="NotSupportedException">
+        /// <paramref name="provider"/> does not contain an input listener requested by the
+        /// <see cref="IInputMapper"/>.
+        /// </exception>
+        void Disconnect(IInputProvider provider);
     }
 }
