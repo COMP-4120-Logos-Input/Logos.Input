e1827efac88e58149e122aecb9e232537f28655f
PixelRican <rimercado02@gmail.com>
2026-04-03 16:20:21 -0400

    Update MouseMotionEventArgs.cs

--- Source/MouseMotionEventArgs.cs
+++ Source/MouseMotionEventArgs.cs
@@ -1,17 +1,49 @@
-﻿using System.Numerics;
+﻿using System;
+using System.Numerics;
 
 namespace Logos.Input
 {
-    public readonly record struct MouseMotionEventArgs
+    /// <summary>
+    /// Provides data for mouse motion events sent by mouse devices.
+    /// </summary>
+    public class MouseMotionEventArgs : InputEventArgs
     {
-        public MouseMotionEventArgs(Vector2 position, long timestamp)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MouseMotionEventArgs"/> class that contains
+        /// event data sent by the specified mouse device at the specified time, which was triggered
+        /// by the specified change in the mouse device's position.
+        /// </summary>
+        /// <param name="device">
+        /// The mouse device that sent the input event.
+        /// </param>
+        /// <param name="timestamp">
+        /// The time at which the input event occured.
+        /// </param>
+        /// <param name="translation">
+        /// The change in the position of <paramref name="device"/>.
+        /// </param>
+        public MouseMotionEventArgs(IMouseDevice device, TimeSpan timestamp, Vector2 translation) : base(device, timestamp)
         {
-            Position = position;
-            Timestamp = timestamp;
+            Translation = translation;
         }
 
-        public Vector2 Position { get; }
+        /// <summary>
+        /// Gets the mouse device that sent the event data.
+        /// </summary>
+        /// <returns>
+        /// The mouse device that sent the event data.
+        /// </returns>
+        public new IMouseDevice Device
+        {
+            get => (IMouseDevice)base.Device;
+        }
 
-        public long Timestamp { get; }
+        /// <summary>
+        /// The change in the position of <see cref="Device"/> when the input event occured.
+        /// </summary>
+        /// <returns>
+        /// The change in the position of <see cref="Device"/> when the input event occured.
+        /// </returns>
+        public Vector2 Translation { get; }
     }
 }
