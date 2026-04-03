c857c4f6593868716cfa1f6030fa665e122e1a0c
PixelRican <rimercado02@gmail.com>
2026-04-03 16:27:58 -0400

    Update MouseWheelEventArgs.cs

--- Source/MouseWheelEventArgs.cs
+++ Source/MouseWheelEventArgs.cs
@@ -1,17 +1,51 @@
-﻿using System.Numerics;
+﻿using System;
+using System.Numerics;
 
 namespace Logos.Input
 {
-    public readonly record struct MouseWheelEventArgs
+    /// <summary>
+    /// Provides data for scroll wheel events sent by mouse devices.
+    /// </summary>
+    public class MouseWheelEventArgs : InputEventArgs
     {
-        public MouseWheelEventArgs(Vector2 rotation, long timestamp)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MouseWheelEventArgs"/> class that contains
+        /// event data sent by the specified mouse device at the specified time, which was triggered
+        /// by the specified change in the mouse device's scroll wheel rotation.
+        /// </summary>
+        /// <param name="device">
+        /// The mouse device that sent the input event.
+        /// </param>
+        /// <param name="timestamp">
+        /// The time at which the input event occured.
+        /// </param>
+        /// <param name="rotation">
+        /// The change in the scroll wheel's rotation on <paramref name="device"/>.
+        /// </param>
+        public MouseWheelEventArgs(IMouseDevice device, TimeSpan timestamp, Vector2 rotation) : base(device, timestamp)
         {
             Rotation = rotation;
-            Timestamp = timestamp;
         }
 
-        public Vector2 Rotation { get; }
+        /// <summary>
+        /// Gets the mouse device that sent the event data.
+        /// </summary>
+        /// <returns>
+        /// The mouse device that sent the event data.
+        /// </returns>
+        public new IMouseDevice Device
+        {
+            get => (IMouseDevice)base.Device;
+        }
 
-        public long Timestamp { get; }
+        /// <summary>
+        /// The change in the scroll wheel's rotation on <see cref="Device"/> when the input event
+        /// occured.
+        /// </summary>
+        /// <returns>
+        /// The change in the scroll wheel's rotation on <see cref="Device"/> when the input event
+        /// occured.
+        /// </returns>
+        public Vector2 Rotation { get; }
     }
 }
