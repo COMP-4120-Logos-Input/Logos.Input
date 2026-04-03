f02c1a86018b1017c6c91922fc87ae18d412e053
PixelRican <rimercado02@gmail.com>
2026-04-03 16:43:54 -0400

    Update IMouseDevice.cs

--- Source/IMouseDevice.cs
+++ Source/IMouseDevice.cs
@@ -1,22 +1,49 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Numerics;
 
 namespace Logos.Input
 {
+    /// <summary>
+    /// Defines methods that query the state of a mouse device.
+    /// </summary>
     public interface IMouseDevice : IInputDevice
     {
-        Vector2 CursorPosition { get; }
+        /// <summary>
+        /// Gets the position of the <see cref="IMouseDevice"/>.
+        /// </summary>
+        /// <returns>
+        /// The position of the <see cref="IMouseDevice"/>.
+        /// </returns>
+        Vector2 Position { get; }
 
-        Vector2 WheelRotation { get; }
+        /// <summary>
+        /// Gets the value of the scroll wheel on the <see cref="IMouseDevice"/>.
+        /// </summary>
+        /// <returns>
+        /// The value of the scroll wheel on the <see cref="IMouseDevice"/>.
+        /// </returns>
+        Vector2 ScrollWheel { get; }
 
-        event EventHandler<MouseButtonEventArgs> ButtonPressed;
+        /// <summary>
+        /// Gets a collection of mouse buttons pressed on the <see cref="IMouseDevice"/>.
+        /// </summary>
+        /// <returns>
+        /// A collection of mouse buttons pressed on the <see cref="IMouseDevice"/>.
+        /// </returns>
+        IEnumerable<MouseButton> PressedButtons { get; }
 
-        event EventHandler<MouseButtonEventArgs> ButtonReleased;
-
-        event EventHandler<MouseWheelEventArgs> WheelRolled;
-
-        event EventHandler<MouseMotionEventArgs> CursorMoved;
-
+        /// <summary>
+        /// Returns a value that indicates whether the specified mouse button is pressed on the
+        /// <see cref="IMouseDevice"/>.
+        /// </summary>
+        /// <param name="button">
+        /// The mouse button to check.
+        /// </param>
+        /// <returns>
+        /// <see langword="true"/> if <paramref name="button"/> is pressed on the
+        /// <see cref="IMouseDevice"/>; otherwise, <see langword="false"/>.
+        /// </returns>
         bool IsButtonPressed(MouseButton button);
     }
 }
