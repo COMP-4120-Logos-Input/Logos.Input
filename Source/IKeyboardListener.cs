d03a37752e07374d28dab5b4515917d35483afc8
PixelRican <rimercado02@gmail.com>
2026-04-03 15:03:15 -0400

    Create IKeyboardListener.cs

---
+++ Source/IKeyboardListener.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Logos.Input
+{
+    /// <summary>
+    /// Defines methods that access connected keyboard devices and notify event handlers when their
+    /// states have changed.
+    /// </summary>
+    public interface IKeyboardListener : IInputListener
+    {
+        /// <summary>
+        /// Gets a collection of keyboard devices that are currently connected.
+        /// </summary>
+        /// <returns>
+        /// A collection of keyboard devices that are currently connected.
+        /// </returns>
+        new IEnumerable<IKeyboardDevice> ConnectedDevices { get; }
+
+        /// <summary>
+        /// Occurs when a key is pressed on a keyboard device.
+        /// </summary>
+        event EventHandler<KeyboardEventArgs> KeyPressed;
+
+        /// <summary>
+        /// Occurs when a key is held down on a keyboard device.
+        /// </summary>
+        event EventHandler<KeyboardEventArgs> KeyRepeated;
+
+        /// <summary>
+        /// Occurs when a key is released on a keyboard device.
+        /// </summary>
+        event EventHandler<KeyboardEventArgs> KeyReleased;
+    }
+}
