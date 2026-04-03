e426faafc802c0af8eeed242c7316630b7ae650a
PixelRican <rimercado02@gmail.com>
2026-04-03 16:34:26 -0400

    Update IKeyboardDevice.cs

--- Source/IKeyboardDevice.cs
+++ Source/IKeyboardDevice.cs
@@ -1,16 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace Logos.Input
 {
+    /// <summary>
+    /// Defines methods that query the state of a keyboard device.
+    /// </summary>
     public interface IKeyboardDevice : IInputDevice
     {
+        /// <summary>
+        /// Gets a collection of keys pressed on the <see cref="IKeyboardDevice"/>.
+        /// </summary>
+        /// <returns>
+        /// A collection of keys pressed on the <see cref="IKeyboardDevice"/>.
+        /// </returns>
         IEnumerable<KeyCode> PressedKeys { get; }
 
-        event EventHandler<KeyEventArgs> KeyPressed;
-
-        event EventHandler<KeyEventArgs> KeyReleased;
-
+        /// <summary>
+        /// Returns a value that indicates whether the specified key is pressed on the
+        /// <see cref="IKeyboardDevice"/>.
+        /// </summary>
+        /// <param name="key">
+        /// The key to check.
+        /// </param>
+        /// <returns>
+        /// <see langword="true"/> if <paramref name="key"/> is pressed on the
+        /// <see cref="IKeyboardDevice"/>; otherwise, <see langword="false"/>.
+        /// </returns>
         bool IsKeyPressed(KeyCode key);
     }
 }
