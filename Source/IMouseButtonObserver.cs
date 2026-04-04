d530847224ed3d0f1720cd2774bd33728e9a5483
PixelRican <rimercado02@gmail.com>
2026-04-04 14:52:36 -0400

    Create IMouseButtonObserver.cs

---
+++ Source/IMouseButtonObserver.cs
@@ -0,0 +1,30 @@
+﻿namespace Logos.Input
+{
+    /// <summary>
+    /// Provides mechanisms for receiving mouse button event data.
+    /// </summary>
+    public interface IMouseButtonObserver
+    {
+        /// <summary>
+        /// Notifies the <see cref="IMouseButtonObserver"/> of a mouse button press event.
+        /// </summary>
+        /// <param name="sender">
+        /// The source of the event.
+        /// </param>
+        /// <param name="e">
+        /// An object that contains mouse button event data.
+        /// </param>
+        void OnButtonPressed(object? sender, MouseButtonEventArgs e);
+
+        /// <summary>
+        /// Notifies the <see cref="IMouseButtonObserver"/> of a mouse button release event.
+        /// </summary>
+        /// <param name="sender">
+        /// The source of the event.
+        /// </param>
+        /// <param name="e">
+        /// An object that contains mouse button event data.
+        /// </param>
+        void OnButtonReleased(object? sender, MouseButtonEventArgs e);
+    }
+}
