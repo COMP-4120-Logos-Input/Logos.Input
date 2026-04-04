a5a009a07227ea1307f6609b89a552e95cf1158b
PixelRican <rimercado02@gmail.com>
2026-04-04 15:35:00 -0400

    Create MouseMotionDirection.cs

---
+++ Source/MouseMotionDirection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Logos.Input
+{
+    /// <summary>
+    /// Specifies bitflags for mouse motion directions. 
+    /// </summary>
+    [Flags]
+    public enum MouseMotionDirection
+    {
+        Any = 0,
+        Up = 1 << 0,
+        Down = 1 << 1,
+        Left = 1 << 2,
+        Right = 1 << 3
+    }
+}
