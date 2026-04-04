759eb4218f4f0e6b5f6821ced2e6a5653db49f94
PixelRican <rimercado02@gmail.com>
2026-04-04 14:38:19 -0400

    Create MouseButtonAction.cs

---
+++ Source/MouseButtonAction.cs
@@ -0,0 +1,11 @@
+﻿namespace Logos.Input
+{
+    /// <summary>
+    /// Specifies mouse button actions.
+    /// </summary>
+    public enum MouseButtonAction
+    {
+        Press,
+        Release
+    }
+}
