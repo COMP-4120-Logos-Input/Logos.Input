7573493a43d55ac52f0bb2768716e0cf278e429f
PixelRican <rimercado02@gmail.com>
2026-04-04 14:59:53 -0400

    Create IMouseMotionObserver.cs

---
+++ Source/IMouseMotionObserver.cs
@@ -0,0 +1,19 @@
+﻿namespace Logos.Input
+{
+    /// <summary>
+    /// Provides mechanisms for receiving mouse motion event data.
+    /// </summary>
+    public interface IMouseMotionObserver
+    {
+        /// <summary>
+        /// Notifies the <see cref="IMouseMotionObserver"/> of a mouse move event.
+        /// </summary>
+        /// <param name="sender">
+        /// The source of the event.
+        /// </param>
+        /// <param name="e">
+        /// An object that contains mouse motion event data.
+        /// </param>
+        void OnMouseMoved(object? sender, MouseMotionEventArgs e);
+    }
+}
