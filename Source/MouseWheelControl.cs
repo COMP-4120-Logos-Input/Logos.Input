5f9f54090977db7f85c17b068f54d3a10c302249
PixelRican <rimercado02@gmail.com>
2026-04-04 16:49:51 -0400

    Create MouseWheelControl.cs

---
+++ Source/MouseWheelControl.cs
@@ -0,0 +1,23 @@
+﻿namespace Logos.Input
+{
+    /// <summary>
+    /// Represents a base class for classes that represent mouse wheel controls whose states can be
+    /// changed by mouse wheel events.
+    /// </summary>
+    /// <typeparam name="T">
+    /// The type of the state associated with the <see cref="MouseWheelControl{T}"/>.
+    /// </typeparam>
+    public abstract class MouseWheelControl<T> : InputControl<T>, IMouseWheelObserver
+    {
+        /// <summary>
+        /// Notifies the <see cref="MouseWheelControl{T}"/> of a mouse wheel move event.
+        /// </summary>
+        /// <param name="sender">
+        /// The source of the event.
+        /// </param>
+        /// <param name="e">
+        /// An object that contains mouse wheel event data.
+        /// </param>
+        public abstract void OnWheelMoved(object? sender, MouseWheelEventArgs e);
+    }
+}
