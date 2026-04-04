56445ab1f1e1dec73f399f99c7ece8ec57fb9352
PixelRican <rimercado02@gmail.com>
2026-04-04 14:56:35 -0400

    Create IMouseWheelObserver.cs

---
+++ Source/IMouseWheelObserver.cs
@@ -0,0 +1,19 @@
+﻿namespace Logos.Input
+{
+    /// <summary>
+    /// Provides mechanisms for receiving mouse wheel event data.
+    /// </summary>
+    public interface IMouseWheelObserver
+    {
+        /// <summary>
+        /// Notifies the <see cref="IMouseWheelObserver"/> of a mouse wheel move event.
+        /// </summary>
+        /// <param name="sender">
+        /// The source of the event.
+        /// </param>
+        /// <param name="e">
+        /// An object that contains mouse wheel event data.
+        /// </param>
+        void OnWheelMoved(object? sender, MouseButtonEventArgs e);
+    }
+}
