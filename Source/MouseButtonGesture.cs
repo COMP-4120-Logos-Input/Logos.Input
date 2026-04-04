9951029a1fff190312bcaf5d4bd994641ab83f1a
PixelRican <rimercado02@gmail.com>
2026-04-04 14:45:02 -0400

    Create MouseButtonGesture.cs

---
+++ Source/MouseButtonGesture.cs
@@ -0,0 +1,40 @@
+﻿namespace Logos.Input
+{
+    /// <summary>
+    /// Represents an association between mouse buttons and mouse button actions.
+    /// </summary>
+    public readonly record struct MouseButtonGesture
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MouseButtonGesture"/> structure to the
+        /// specified mouse button and action.
+        /// </summary>
+        /// <param name="button">
+        /// The mouse button associated with the <see cref="MouseButtonGesture"/>.
+        /// </param>
+        /// <param name="action">
+        /// The action associated with the <see cref="MouseButtonGesture"/>.
+        /// </param>
+        public MouseButtonGesture(MouseButton button, MouseButtonAction action)
+        {
+            Button = button;
+            Action = action;
+        }
+
+        /// <summary>
+        /// Gets the mouse button associated with the <see cref="MouseButtonGesture"/>.
+        /// </summary>
+        /// <returns>
+        /// The mouse button associated with the <see cref="MouseButtonGesture"/>.
+        /// </returns>
+        public MouseButton Button { get; }
+
+        /// <summary>
+        /// Gets the action associated with the <see cref="MouseButtonGesture"/>.
+        /// </summary>
+        /// <returns>
+        /// The action associated with the <see cref="MouseButtonGesture"/>.
+        /// </returns>
+        public MouseButtonAction Action { get; }
+    }
+}
