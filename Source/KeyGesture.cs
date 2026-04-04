049c570f9f648c3dba697da5b450433af88e4e03
PixelRican <rimercado02@gmail.com>
2026-04-04 13:34:41 -0400

    Create KeyGesture.cs

---
+++ Source/KeyGesture.cs
@@ -0,0 +1,40 @@
+﻿namespace Logos.Input
+{
+    /// <summary>
+    /// Defines a pairing of keys and actions.
+    /// </summary>
+    public readonly record struct KeyGesture
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="KeyGesture"/> structure to the specified
+        /// key and action.
+        /// </summary>
+        /// <param name="key">
+        /// The key associated with the <see cref="KeyGesture"/>.
+        /// </param>
+        /// <param name="action">
+        /// The action associated with the <see cref="KeyGesture"/>.
+        /// </param>
+        public KeyGesture(KeyCode key, KeyAction action)
+        {
+            Key = key;
+            Action = action;
+        }
+
+        /// <summary>
+        /// Gets the key associated with the <see cref="KeyGesture"/>.
+        /// </summary>
+        /// <returns>
+        /// The key associated with the <see cref="KeyGesture"/>.
+        /// </returns>
+        public KeyCode Key { get; }
+
+        /// <summary>
+        /// Gets the action associated with the <see cref="KeyGesture"/>.
+        /// </summary>
+        /// <returns>
+        /// The action associated with the <see cref="KeyGesture"/>.
+        /// </returns>
+        public KeyAction Action { get; }
+    }
+}
