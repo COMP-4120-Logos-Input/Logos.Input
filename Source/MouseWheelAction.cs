dcfc9e3e43a24dd7b82ab3fdbbf1e264c82dcc16
PixelRican <rimercado02@gmail.com>
2026-04-04 14:39:22 -0400

    Create MouseWheelAction.cs

---
+++ Source/MouseWheelAction.cs
@@ -0,0 +1,13 @@
+﻿namespace Logos.Input
+{
+    /// <summary>
+    /// Specifies mouse wheel actions.
+    /// </summary>
+    public enum MouseWheelAction
+    {
+        ScrollUp,
+        ScrollDown,
+        ScrollLeft,
+        ScrollRight,
+    }
+}
