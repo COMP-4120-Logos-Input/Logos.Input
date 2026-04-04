8b5e8642beb2bcbd97e078684671e225be7fd7ec
PixelRican <rimercado02@gmail.com>
2026-04-04 16:11:50 -0400

    Update MouseMapper.cs

--- Source/MouseMapper.cs
+++ Source/MouseMapper.cs
@@ -1,57 +1,128 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Numerics;
 
 namespace Logos.Input
 {
+    /// <summary>
+    /// Represents an input mapper that routes input events triggered by specific mouse gestures to
+    /// mouse observers.
+    /// </summary>
     public class MouseMapper : IInputMapper
     {
-        private readonly Dictionary<MouseButtonGesture, EventHandler<MouseButtonEventArgs>> _buttonBindings;
-        private EventHandler<MouseMotionEventArgs>? _cursorBinding;
-        private EventHandler<MouseWheelEventArgs>? _wheelBinding;
+        private const int UpDirectionFlag = 1 << 0;
+        private const int DownDirectionFlag = 1 << 1;
+        private const int LeftDirectionFlag = 1 << 2;
+        private const int RightDirectionFlag = 1 << 3;
 
+        private readonly Dictionary<MouseButtonGesture, IMouseButtonObserver> _buttonBindings;
+        private readonly Dictionary<MouseMotionDirection, IMouseMotionObserver> _motionBindings;
+        private readonly Dictionary<MouseWheelDirection, IMouseWheelObserver> _wheelBindings;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MouseMapper"/> class.
+        /// </summary>
         public MouseMapper()
         {
-            _buttonBindings = new Dictionary<MouseButtonGesture, EventHandler<MouseButtonEventArgs>>();
+            _buttonBindings = new Dictionary<MouseButtonGesture, IMouseButtonObserver>();
+            _motionBindings = new Dictionary<MouseMotionDirection, IMouseMotionObserver>();
+            _wheelBindings = new Dictionary<MouseWheelDirection, IMouseWheelObserver>();
         }
 
-        public void BindButtonPress(MouseButton button, EventHandler<MouseButtonEventArgs> handler)
+        /// <summary>
+        /// Routes input events triggered by the specified mouse button gesture to the specified
+        /// mouse button observer.
+        /// </summary>
+        /// <param name="gesture">
+        /// The mouse button gesture to bind.
+        /// </param>
+        /// <param name="observer">
+        /// The mouse button observer that will listen for input events triggered by
+        /// <paramref name="gesture"/>.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="observer"/> is <see langword="null"/>.
+        /// </exception>
+        public void Bind(MouseButtonGesture gesture, IMouseButtonObserver observer)
         {
-            BindEvent(button, handler, MouseButtonEventType.Press);
+            ArgumentNullException.ThrowIfNull(observer);
+            _buttonBindings.Add(gesture, observer);
         }
 
-        public void BindButtonRelease(MouseButton button, EventHandler<MouseButtonEventArgs> handler)
+        /// <summary>
+        /// Routes input events triggered by the specified mouse motion direction to the specified
+        /// mouse motion observer.
+        /// </summary>
+        /// <param name="direction">
+        /// The mouse motion direction to bind.
+        /// </param>
+        /// <param name="observer">
+        /// The mouse motion observer that will listen for input events triggered by mouse motion
+        /// in <paramref name="direction"/>.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="observer"/> is <see langword="null"/>.
+        /// </exception>
+        public void Bind(MouseMotionDirection direction, IMouseMotionObserver observer)
         {
-            BindEvent(button, handler, MouseButtonEventType.Release);
+            ArgumentNullException.ThrowIfNull(observer);
+            _motionBindings.Add(direction, observer);
         }
 
-        public void BindMouseMove(EventHandler<MouseMotionEventArgs> handler)
+        /// <summary>
+        /// Routes input events triggered by the specified mouse wheel scroll direction to the
+        /// specified mouse wheel observer.
+        /// </summary>
+        /// <param name="direction">
+        /// The mouse wheel scroll direction to bind.
+        /// </param>
+        /// <param name="observer">
+        /// The mouse wheel observer that will listen for input events triggered by mouse wheel
+        /// scrolling in <paramref name="direction"/>.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="observer"/> is <see langword="null"/>.
+        /// </exception>
+        public void Bind(MouseWheelDirection direction, IMouseWheelObserver observer)
         {
-            _cursorBinding += handler;
+            ArgumentNullException.ThrowIfNull(observer);
+            _wheelBindings.Add(direction, observer);
         }
 
-        public void BindWheelMove(EventHandler<MouseWheelEventArgs> handler)
+        /// <summary>
+        /// Removes the mouse button observer that was listening for input events triggered by the
+        /// specified mouse button gesture.
+        /// </summary>
+        /// <param name="gesture">
+        /// The mouse button gesture to unbind.
+        /// </param>
+        public void Unbind(MouseButtonGesture gesture)
         {
-            _wheelBinding += handler;
+            _buttonBindings.Remove(gesture);
         }
 
-        public void UnbindButtonPress(MouseButton button)
+        /// <summary>
+        /// Removes the mouse motion observer that was listening for input events triggered by mouse
+        /// motion in the specified direction.
+        /// </summary>
+        /// <param name="direction">
+        /// The mouse wheel motion direction to unbind.
+        /// </param>
+        public void Unbind(MouseMotionDirection direction)
         {
-            UnbindEvent(button, MouseButtonEventType.Press);
+            _motionBindings.Remove(direction);
         }
 
-        public void UnbindButtonRelease(MouseButton button)
+        /// <summary>
+        /// Removes the mouse wheel observer that was listening for input events triggered by mouse
+        /// wheel scrolling in the specified direction.
+        /// </summary>
+        /// <param name="direction">
+        /// The mouse wheel scroll direction to unbind.
+        /// </param>
+        public void Unbind(MouseWheelDirection direction)
         {
-            UnbindEvent(button, MouseButtonEventType.Release);
-        }
-
-        public void UnbindCursorMotion()
-        {
-            _cursorBinding = null;
-        }
-
-        public void UnbindWheelRotation()
-        {
-            _wheelBinding = null;
+            _wheelBindings.Remove(direction);
         }
 
         public void Connect(IInputProvider provider)
@@ -78,17 +149,6 @@ namespace Logos.Input
             BlockEvents(provider.GetListener<IMouseListener>());
         }
 
-        private void BindEvent(MouseButton button, EventHandler<MouseButtonEventArgs> handler, MouseButtonEventType type)
-        {
-            ArgumentNullException.ThrowIfNull(handler);
-            _buttonBindings[new MouseButtonGesture(button, type)] = handler;
-        }
-
-        private void UnbindEvent(MouseButton button, MouseButtonEventType type)
-        {
-            _buttonBindings.Remove(new MouseButtonGesture(button, type));
-        }
-
         private void RouteEvents(IMouseListener listener)
         {
             listener.ButtonPressed += OnButtonPressed;
@@ -107,50 +167,77 @@ namespace Logos.Input
 
         private void OnButtonPressed(object? sender, MouseButtonEventArgs args)
         {
-            OnButtonEvent(sender, args, MouseButtonEventType.Press);
+            MouseButtonGesture gesture = new MouseButtonGesture(args.Button, MouseButtonAction.Press);
+
+            if (_buttonBindings.TryGetValue(gesture, out IMouseButtonObserver? observer))
+            {
+                observer.OnButtonPressed(sender, args);
+            }
         }
 
         private void OnButtonReleased(object? sender, MouseButtonEventArgs args)
         {
-            OnButtonEvent(sender, args, MouseButtonEventType.Release);
-        }
+            MouseButtonGesture gesture = new MouseButtonGesture(args.Button, MouseButtonAction.Release);
 
-        private void OnButtonEvent(object? sender, MouseButtonEventArgs args, MouseButtonEventType type)
-        {
-            MouseButtonGesture gesture = new MouseButtonGesture(args.Button, type);
-
-            if (_buttonBindings.TryGetValue(gesture, out EventHandler<MouseButtonEventArgs>? handler))
+            if (_buttonBindings.TryGetValue(gesture, out IMouseButtonObserver? observer))
             {
-                handler(sender, args);
+                observer.OnButtonReleased(sender, args);
             }
         }
 
         private void OnMouseMoved(object? sender, MouseMotionEventArgs args)
         {
-            _cursorBinding?.Invoke(sender, args);
+            if (_motionBindings.TryGetValue(MouseMotionDirection.Any, out IMouseMotionObserver? observer))
+            {
+                observer.OnMouseMoved(sender, args);
+            }
+
+            MouseMotionDirection direction = (MouseMotionDirection)ToDirectionFlags(args.Translation);
+
+            if (direction != MouseMotionDirection.Any && _motionBindings.TryGetValue(direction, out observer))
+            {
+                observer.OnMouseMoved(sender, args);
+            }
         }
 
         private void OnWheelMoved(object? sender, MouseWheelEventArgs args)
         {
-            _wheelBinding?.Invoke(sender, args);
-        }
-
-        private readonly record struct MouseButtonGesture
-        {
-            public readonly MouseButton Button;
-            public readonly MouseButtonEventType Type;
-
-            public MouseButtonGesture(MouseButton key, MouseButtonEventType type)
+            if (_wheelBindings.TryGetValue(MouseWheelDirection.Any, out IMouseWheelObserver? observer))
             {
-                Button = key;
-                Type = type;
+                observer.OnWheelMoved(sender, args);
+            }
+
+            MouseWheelDirection direction = (MouseWheelDirection)ToDirectionFlags(args.Delta);
+
+            if (direction != MouseWheelDirection.Any && _wheelBindings.TryGetValue(direction, out observer))
+            {
+                observer.OnWheelMoved(sender, args);
             }
         }
 
-        private enum MouseButtonEventType
+        private int ToDirectionFlags(Vector2 direction)
         {
-            Press,
-            Release
+            int flags = 0;
+
+            if (direction.Y > 0.0f)
+            {
+                flags |= UpDirectionFlag;
+            }
+            else if (direction.Y < 0.0f)
+            {
+                flags |= DownDirectionFlag;
+            }
+            
+            if (direction.X > 0.0f)
+            {
+                flags |= LeftDirectionFlag;
+            }
+            else if (direction.X < 0.0f)
+            {
+                flags |= RightDirectionFlag;
+            }
+
+            return flags;
         }
     }
 }
