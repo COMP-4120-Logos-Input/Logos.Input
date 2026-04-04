adbc72e32bcd1a6bdc59cad9112019e08a1ed4d2
PixelRican <rimercado02@gmail.com>
2026-04-04 13:25:12 -0400

    Create IKeyObserver.cs

---
+++ Source/IKeyObserver.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Logos.Input
+{
+    /// <summary>
+    /// Provides mechanisms for receiving key event data.
+    /// </summary>
+    public interface IKeyObserver
+    {
+        /// <summary>
+        /// Notifies the <see cref="IKeyObserver"/> of a key press event.
+        /// </summary>
+        /// <param name="sender">
+        /// The source of the event.
+        /// </param>
+        /// <param name="e">
+        /// An object that contains key event data.
+        /// </param>
+        void OnKeyPressed(object? sender, KeyEventArgs e);
+
+        /// <summary>
+        /// Notifies the <see cref="IKeyObserver"/> of a key repeat event.
+        /// </summary>
+        /// <param name="sender">
+        /// The source of the event.
+        /// </param>
+        /// <param name="e">
+        /// An object that contains key event data.
+        /// </param>
+        void OnKeyRepeated(object? sender, KeyEventArgs e);
+
+        /// <summary>
+        /// Notifies the <see cref="IKeyObserver"/> of a key release event.
+        /// </summary>
+        /// <param name="sender">
+        /// The source of the event.
+        /// </param>
+        /// <param name="e">
+        /// An object that contains key event data.
+        /// </param>
+        void OnKeyReleased(object? sender, KeyEventArgs e);
+    }
+}
