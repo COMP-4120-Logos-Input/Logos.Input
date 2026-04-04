6e4ac2d2d201d692baaf8584f0c7641b6c6bd2ec
Jumony <fireball2489@gmail.com>
2026-04-04 11:20:38 -0400

    SDL3 P/Invoke Additions

--- Source/Sdl3/SDL3.cs
+++ Source/Sdl3/SDL3.cs
@@ -30,5 +30,17 @@ namespace Logos.Input.Sdl3
         [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
         [return: MarshalAs(UnmanagedType.U1)]
         public static partial bool SDL_PushEvent(ref SDL_Event e);
+        
+        [LibraryImport(nameof(SDL3), StringMarshalling = StringMarshalling.Utf8)]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        public static partial nint SDL_CreateWindow(string title, int w, int h, SDL_WindowFlags flags);
+
+        [LibraryImport(nameof(SDL3))]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        public static partial void SDL_DestroyWindow(nint window);
+
+        [LibraryImport(nameof(SDL3))]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        public static partial uint SDL_GetWindowID(nint window);
     }
 }
