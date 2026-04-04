156ad200ff9e263938f6decdedad9aff12bf6945
PixelRican <rimercado02@gmail.com>
2026-04-04 15:34:41 -0400

    Remove to MouseWheelDirection

---
+++ Source/MouseWheelDirection.cs
@@ -0,0 +1,14 @@
+﻿namespace Logos.Input
+{
+    /// <summary>
+    /// Specifies mouse wheel directions.
+    /// </summary>
+    public enum MouseWheelDirection
+    {
+        Any,
+        Up,
+        Down,
+        Left,
+        Right
+    }
+}
