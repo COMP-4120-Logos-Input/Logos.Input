bb08f7ba76c383fbcba600d9400db32c743bf8c3
PixelRican <rimercado02@gmail.com>
2026-04-04 20:42:26 -0400

    Update documentation

--- Source/IInputMapper.cs
+++ Source/IInputMapper.cs
@@ -8,8 +8,8 @@ namespace Logos.Input
     public interface IInputMapper
     {
         /// <summary>
-        /// Routes events exposed by compatible input listeners contained by the specified input
-        /// provider to the <see cref="IInputMapper"/>.
+        /// Routes events exposed by compatible input listeners from the specified input provider to
+        /// the <see cref="IInputMapper"/>.
         /// </summary>
         /// <param name="provider">
         /// The input provider containing input listeners whose events are to be routed to the
@@ -19,14 +19,13 @@ namespace Logos.Input
         /// <paramref name="provider"/> is <see langword="null"/>.
         /// </exception>
         /// <exception cref="NotSupportedException">
-        /// <paramref name="provider"/> does not contain an input listener requested by the
-        /// <see cref="IInputMapper"/>.
+        /// <paramref name="provider"/> does not contain a required input listener.
         /// </exception>
         void RouteEvents(IInputProvider provider);
 
         /// <summary>
-        /// Blocks events exposed by compatible input listeners contained by the specified input
-        /// provider from reaching the <see cref="IInputMapper"/>.
+        /// Blocks events exposed by compatible input listeners from the specified input provider
+        /// from reaching the <see cref="IInputMapper"/>.
         /// </summary>
         /// <param name="provider">
         /// The input provider containing input listeners whose events are to be blocked from
@@ -36,8 +35,7 @@ namespace Logos.Input
         /// <paramref name="provider"/> is <see langword="null"/>.
         /// </exception>
         /// <exception cref="NotSupportedException">
-        /// <paramref name="provider"/> does not contain an input listener requested by the
-        /// <see cref="IInputMapper"/>.
+        /// <paramref name="provider"/> does not contain a required input listener.
         /// </exception>
         void BlockEvents(IInputProvider provider);
     }
